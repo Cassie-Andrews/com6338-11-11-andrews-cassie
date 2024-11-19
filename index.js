6c2bb78115b5b59ab00c31ffdfcf00072241b45b
Cassie Andrews <candrews1@ufl.edu>
2024-11-19 14:30:55 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const getPoemBtn = document.getElementById('get-poem')
+const poemEl = document.getElementById('poem')
+const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json'
+
+const getJSON = url => fetch(url).then(res => res.json())
+
+// helper function #2
+    // Pipe is a standard feature of many functional programming libraries that allows the programmer to chain function calls together while passing the output from one function into the input of another
+const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
+
+// helper function #1
+    // This function is curried to take the tag name as an argument, and then return a function that takes its argument and nests it with the tag. This function can be used in various ways to create html elements for makePoemHTML. Here are some usage examples of makeTag:
+const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+
+// complete this function
+    // makePoemHTML will accept PoetryDB API's response and should output a single string of html. This string should consist of an h2 element containing the title of the poem, an em element containing "by " and the author's name that is itself inside of an h3element, and then paragraph elements for each stanza of the poem that contain lines separated by linebreak tags.
+    // Note that the last line in each paragraph tag does NOT contain a linebreak element after it.
+const makePoemHTML = () => {}
+
+// attach a click event to #get-poem
+getPoemBtn.onclick = async function() {
+  // renders the HTML string returned by makePoemHTML to #poem
+  poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
+}
+
+
+// After clicking the "Get Poem" button, the application should make a call to https://poetrydb.org/random,linecount/1;12/author,title,lines.json
+
+// The app should then take this response and render it inside of the #poem element
+
+// Subsequent clicks of the "Get Poem" button should clear the markup in the #poem element before rendering new poem markup.
+
+// Note: You must use makeTag and pipe at least once in your solution
+// You may also make as many additional helper functions as you wish within your code.
+
+
+
+
+
+// TIPS
+// the split, join, and map functions can make short work of separating the poem into stanzas and inserting linebreaks between the lines inside of stanzas. 
+// Pay extra attention to how the lines property includes "" to separate stanzas and how that should look as HTML in the final app
